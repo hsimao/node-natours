4d779446665f1bf82e64842d5d00c4e4d48412d1
hsimao <e087754958@gmail.com>
2019-09-22 17:48:01 +0800

    add: 使用 mongoose

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 const configName = process.env.NODE_ENV === 'development' ? 'dev' : 'prod';
 dotenv.config({ path: `${__dirname}/config/${configName}.env` });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB conection successful!'));
 
 // Start server
 const port = process.env.PORT || 3000;
