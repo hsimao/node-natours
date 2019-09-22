2617200cae25a1026d3a093acfd239f227f05873
hsimao <e087754958@gmail.com>
2019-09-22 23:55:36 +0800

    edit: 修改tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,60 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: true
+    unique: true,
+    trim: true
   },
-  rating: {
+  // 持續時間
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  // 每團最多人數
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  // 困難度
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty']
+  },
+  // 平均評價分數
+  ratingsAverage: {
     type: Number,
     default: 4.5
+  },
+  // 評價數量
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price']
-  }
+  },
+  // 優惠價
+  priceDiscount: Number,
+  // 摘要
+  summary: {
+    type: String,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cove image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  // 旅行開始時間
+  startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
