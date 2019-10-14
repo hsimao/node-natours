278c6e077c5424e4fb1eee816a6539f7e34949f9
hsimao <e087754958@gmail.com>
2019-10-14 19:41:52 +0800

    add: 未定義 route error handle

--- app.js
+++ app.js
@@ -22,4 +22,12 @@ const userRouter = require('./routes/userRoutes');
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
+// 未定義 route handle
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
+});
+
 module.exports = app;
