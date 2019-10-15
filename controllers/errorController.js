7f3b2972db7ea78a420c4ae137e6e2021ffacb9f
hsimao <e087754958@gmail.com>
2019-10-15 21:13:57 +0800

    add: handle mongoDB error, 無效 ID

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,12 @@
+const AppError = require('./../utils/appError');
+
+// handle mongoDB error
+const handleCastErrorDB = err => {
+  // 無效 id
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -36,6 +45,9 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    // handle mongoDB error
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
