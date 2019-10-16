ea997d97a3d929cf3bd993dc71e7ddbc5aef903a
hsimao <e087754958@gmail.com>
2019-10-16 19:55:22 +0800

    add: 新增全域捕獲未處理的錯誤

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// 捕獲未處理的 error
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('Uncaught Exception! Shutting down...');
+  process.exit(1); // 0 表示成功, 1 表示失敗
+});
+
 const configName = process.env.NODE_ENV === 'development' ? 'dev' : 'prod';
 dotenv.config({ path: `${__dirname}/config/${configName}.env` });
 
@@ -26,7 +33,7 @@ const server = app.listen(port, () => {
   console.log(`App start on port ${port}...`);
 });
 
-// 全域監聽事件 - 處理未 handle 的 promise 錯誤 (Unhandled promise rejection)
+// 捕獲未處理的異步 error - 例如某 promise 沒有使用 .catch (Unhandled promise rejection)
 process.on('unhandledRejection', err => {
   console.log(err.name, err.message);
   console.log('Unhandler Rejection! Shutting down...');
