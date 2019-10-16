3fa5133c372be5f0a7e5a5c0d331d15dc6113e6a
hsimao <e087754958@gmail.com>
2019-10-16 20:41:24 +0800

    add: 新增 signup api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const Router = express.Router();
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
+
+const Router = express.Router();
+
+Router.post('/signup', authController.signup);
 
 Router.route('/')
   .get(userController.getAllUsers)
