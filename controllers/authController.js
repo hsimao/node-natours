cf437366bd8ef45603c38d240508ebd83f816486
hsimao <e087754958@gmail.com>
2019-10-16 21:38:07 +0800

    add: 產生 jwt token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
@@ -9,8 +10,14 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm
   });
 
+  // 產生 jwt token
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
