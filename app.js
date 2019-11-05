ab00eb3b0a5076a9232c5c1db25ba8371cbcaebc
hsimao <e087754958@gmail.com>
2019-11-05 21:26:21 +0800

    add: 新增防禦相同 IP 太多請求限制

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-// Middlewares 中間件
+// Global Middlewares 中間件
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// 限制同一個 ip 在時間內僅能發送多少請求
+const limiter = rateLimit({
+  max: 100, // 最大請求數量
+  windowMs: 60 * 60 * 100, // 1 小時
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+// 僅在 /api 路由套用此限制
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
