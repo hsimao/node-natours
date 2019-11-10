ba690474427459ea95b7c04f5923d7d92c5a2728
hsimao <e087754958@gmail.com>
2019-11-10 17:47:15 +0800

    add: 新增評論 data model

---
+++ models/reviewModel.js
@@ -0,0 +1,51 @@
+//  評論 review 資料結構
+// 1.) review
+// 2.) ratting 評分
+// 3.) createdAt 創建時間
+// 4.) 關聯 Tour
+// 5.) 關聯 User
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!'],
+      trim: true,
+      maxlength: [300, 'A review must have less or equal then 300 characters'],
+      minlength: [2, 'A review must have more or equal then 2 characters']
+    },
+    ratting: {
+      type: Number,
+      min: [1, 'Ratting must be above 1'],
+      max: [5, 'Ratting must below 5.0']
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    tour: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour',
+        required: [true, 'Review must belong to a tour.']
+      }
+    ],
+    user: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Review must belong to a user.']
+      }
+    ]
+  },
+  // 設定使用虛擬屬性
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
