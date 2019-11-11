fec065d0e0894feb7e18cc39f2ffc5bd33a27926
hsimao <e087754958@gmail.com>
2019-11-11 20:34:31 +0800

    edit: 優化 tour / review  嵌套路由

--- routes/reviewRouters.js
+++ routes/reviewRouters.js
@@ -2,7 +2,11 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-const Router = express.Router();
+// mergeParams: true => 允許接收其他 router 上的 params, 例如 tourRoutes 的 :tourId
+const Router = express.Router({ mergeParams: true });
+
+// POST /tour/34333/reviews
+// POST /reviews
 
 Router.route('/')
   .get(reviewController.getAllReviews)
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./reviewRouters');
 
 const Router = express.Router();
+
+// 與 reviewRouter 嵌套
+// POST /tour/34333/reviews
+Router.use('/:tourId/reviews', reviewRouter);
 
 // 捕捉 /api/v1/tours 路由中有 :id 參數的請求, 統一處理驗證
 // Router.param('id', tourController.checkID);
@@ -29,12 +33,5 @@ Router.route('/:id')
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
   );
-
-// POST /tour/34333/reviews
-Router.route('/:tourId/reviews').post(
-  authController.protect,
-  authController.restrictTo('user'),
-  reviewController.createReview
-);
 
 module.exports = Router;
