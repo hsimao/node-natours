801a9539bfc105bfa068931cd6bf08528897cbb8
hsimao <e087754958@gmail.com>
2019-11-17 15:39:54 +0800

    add: 拆分 view 資料結構

--- app.js
+++ app.js
@@ -66,29 +66,12 @@ app.use((req, res, next) => {
 });
 
 // Routes
+const viewRouter = require('./routes/viewRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRouters');
 
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The forest',
-    user: 'Jonas'
-  });
-});
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours'
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/reviews', reviewRouter);
