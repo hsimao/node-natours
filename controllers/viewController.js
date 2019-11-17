3fb62e34e1d37231bd9a5f9a6aaf144bc7c2b4c7
hsimao <e087754958@gmail.com>
2019-11-17 19:08:32 +0800

    add: 新增 tour 詳細頁面樣板

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -11,8 +11,14 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res) => {
+  // 取得 tour 詳細頁面所需資料
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+    tour
+  });
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,6 @@ const viewController = require('../controllers/viewController');
 const Router = express.Router();
 
 Router.get('/', viewController.getOverview);
-Router.get('/tour', viewController.getTour);
+Router.get('/tour/:slug', viewController.getTour);
 
 module.exports = Router;
