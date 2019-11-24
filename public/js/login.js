66bf812ecd23078cfec50db943b3fe446a122361
hsimao <e087754958@gmail.com>
2019-11-24 15:07:37 +0800

    add: 使用 parcel 編譯 js, 調整 js 引入方式, 新增 alerts 彈窗方法

--- public/js/login.js
+++ public/js/login.js
@@ -1,36 +1,25 @@
 /* eslint-disable */
-const inputEmail = document.getElementById('email');
-inputEmail.focus();
+import axios from 'axios';
+import { showAlert } from './alerts';
 
-function login(email, password) {
-  axios({
-    method: 'post',
-    url: 'http://localhost:3000/api/v1/users/login',
-    data: {
-      email,
-      password
-    }
-  })
-    .then(res => {
-      console.log(res);
-      if (res.data.status === 'success') {
-        alert('Logged in successfully!');
-        window.setTimeout(() => {
-          location.assign('/');
-        }, 1500);
+export const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: 'post',
+      url: 'http://localhost:3000/api/v1/users/login',
+      data: {
+        email,
+        password
       }
-    })
-    .catch(err => alert(err.response.data.message));
-}
+    });
 
-function handleLoginSubmit() {
-  document.querySelector('.form').addEventListener('submit', e => {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    login(email, password);
-  });
-}
-
-handleLoginSubmit();
+    if (res.data.status === 'success') {
+      showAlert('success', 'Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,52 +1,52 @@
 /* eslint-disable */
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
+export const displayMap = locations => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiaHNpbWFvIiwiYSI6ImNrMzYwODJtNzBueHQzY3Blb2hodXZsMDkifQ.PEJGnj7qoBvoV68p7wQSxw';
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiaHNpbWFvIiwiYSI6ImNrMzYwODJtNzBueHQzY3Blb2hodXZsMDkifQ.PEJGnj7qoBvoV68p7wQSxw';
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/hsimao/ck360c7fw13wa1cmbflkff42r',
+    scrollZoom: false
+    // center: [-118.35607, 34.140441],
+    // zoom: 9
+    // interactive: false // 禁止用戶改變 map
+  });
 
-const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/hsimao/ck360c7fw13wa1cmbflkff42r',
-  scrollZoom: false
-  // center: [-118.35607, 34.140441],
-  // zoom: 9
-  // interactive: false // 禁止用戶改變 map
-});
+  const bounds = new mapboxgl.LngLatBounds();
 
-const bounds = new mapboxgl.LngLatBounds();
+  // loop 所有座標
+  locations.forEach(location => {
+    // 建立地圖座標 dev
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-// loop 所有座標
-locations.forEach(location => {
-  // 建立地圖座標 dev
-  const el = document.createElement('div');
-  el.className = 'marker';
+    // 添加地圖座標 dev 到 mapbox marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    })
+      .setLngLat(location.coordinates)
+      .addTo(map);
 
-  // 添加地圖座標 dev 到 mapbox marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom'
-  })
-    .setLngLat(location.coordinates)
-    .addTo(map);
+    // add popup
+    new mapboxgl.Popup({
+      offset: 30
+    })
+      .setLngLat(location.coordinates)
+      .setHTML(`<p>Day ${location.day}: ${location.description}</p>`)
+      .addTo(map);
 
-  // add popup
-  new mapboxgl.Popup({
-    offset: 30
-  })
-    .setLngLat(location.coordinates)
-    .setHTML(`<p>Day ${location.day}: ${location.description}</p>`)
-    .addTo(map);
+    // 擴充地圖可視範圍範圍, 需包含當前位置
+    bounds.extend(location.coordinates);
+  });
 
-  // 擴充地圖可視範圍範圍, 需包含當前位置
-  bounds.extend(location.coordinates);
-});
-
-// 更新 map 可視範圍
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100
-  }
-});
+  // 更新 map 可視範圍
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100
+    }
+  });
+};
