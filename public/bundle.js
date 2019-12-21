4cbecdf83c47ce1f98cf96d2384bbcba2215f89d
hsimao <e087754958@gmail.com>
2019-12-21 19:30:19 +0800

    edit: 使用 class 重新封裝 email util

--- public/bundle.js
+++ public/bundle.js
@@ -8943,7 +8943,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63924" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50827" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
