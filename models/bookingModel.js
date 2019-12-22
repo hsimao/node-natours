50c7ac1be54883aef715bb39518459943cd6fbcd
hsimao <e087754958@gmail.com>
2019-12-22 15:51:12 +0800

    add: create booking model

---
+++ models/bookingModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Booking must belong to a Tour!']
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Booking must belong to a User!']
+  },
+  price: {
+    type: Number,
+    require: [true, 'Booking must have a price.']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  // 是否已付款
+  paid: {
+    type: Boolean,
+    default: true
+  }
+});
+
+// 當查詢 booking 時, 自動填充 tour、user 相關資訊
+bookingSchema.pre(/^find/, function(next) {
+  this.populate('user').populate({
+    path: 'tour',
+    select: 'name'
+  });
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
